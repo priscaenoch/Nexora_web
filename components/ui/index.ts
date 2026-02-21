dea0752727e59b13f6b0e335f7b9ecf529fe917b
Nabeelahh <saninabeelah@gmail.com>
2026-02-21 17:49:43 +0100

    created reuseable component

---
+++ components/ui/index.ts
@@ -0,0 +1,25 @@
+// Modal components
+export { Modal, ModalHeader, ModalBody, ModalFooter } from "./Modal";
+export type {
+  ModalProps,
+  ModalHeaderProps,
+  ModalBodyProps,
+  ModalFooterProps,
+  ModalVariant,
+  ModalSize,
+} from "./Modal";
+
+// Spinner components
+export { Spinner } from "./Spinner";
+export type { SpinnerProps, SpinnerSize, SpinnerColor } from "./Spinner";
+
+// Toast components
+export { Toast, ToastProvider, useToast } from "./Toast";
+export type {
+  ToastProps,
+  ToastType,
+  ToastPosition,
+  ToastDuration,
+  ToastContextType,
+  ToastProviderProps,
+} from "./Toast";
