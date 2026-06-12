951174756402e2568b8fdb08e26c19a7154fdaf5
Alqku <ibrahimmosouf@gmail.com>
2026-06-12 22:52:37 +0000

    chore: rebrand sitemap and robots base URL

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://stellaraid.com'
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://orbitchain.com'
 
   return {
     rules: {
